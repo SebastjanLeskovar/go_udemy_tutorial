dfcad2d7496bdff2cf986aaf09efcec8fae87af4
Sebastjan Leskovar <sebastjan.leskovar@gmail.com>
2020-11-08 20:23:38 +0100

    3-even_and_odd

---
+++ 3_cards/Even and Odd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	numbers := []int{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	}
+
+	for _, n := range numbers{
+		if n % 2 == 0 {
+			fmt.Printf("Number %d is even.\n", n)
+		} else {
+			fmt.Printf("Number %d is odd.\n", n)
+		}
+	}
+}
